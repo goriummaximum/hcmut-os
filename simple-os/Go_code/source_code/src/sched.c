280649af525f5a447059677b1960ffc7b0d9cf66
thiengo <long.hothiengo@hcmut.edu.vn>
2021-05-20 18:40:44 +0700

    update-my-implementation-ae-tham-khao-neu-co-nhu-cau

---
+++ simple-os/Go_code/source_code/src/sched.c
@@ -0,0 +1,59 @@
+
+#include "queue.h"
+#include "sched.h"
+#include <pthread.h>
+
+static struct queue_t ready_queue;
+static struct queue_t run_queue;
+static pthread_mutex_t queue_lock;
+
+int queue_empty(void) {
+	return (empty(&ready_queue) && empty(&run_queue));
+}
+
+void init_scheduler(void) {
+	ready_queue.size = 0;
+	run_queue.size = 0;
+	pthread_mutex_init(&queue_lock, NULL);
+}
+
+struct pcb_t * get_proc(void) {
+	struct pcb_t * proc = NULL;
+	/*TODO: get a process from [ready_queue]. If ready queue
+	 * is empty, push all processes in [run_queue] back to
+	 * [ready_queue] and return the highest priority one.
+	 * Remember to use lock to protect the queue.
+	 * */
+	
+	//process ready queue empty
+	if (empty(&ready_queue))
+	{
+		for (int i = 0; i < run_queue.size; i++)
+		{
+			add_proc(run_queue.proc[i]);
+			run_queue.proc[i] = NULL;
+		}
+		run_queue.size = 0;
+	}
+
+	//get highest priority proc from ready queue
+	pthread_mutex_lock(&queue_lock);
+	proc = dequeue(&ready_queue);
+	pthread_mutex_unlock(&queue_lock);
+	
+	return proc;
+}
+
+void put_proc(struct pcb_t * proc) {
+	pthread_mutex_lock(&queue_lock);
+	enqueue(&run_queue, proc);
+	pthread_mutex_unlock(&queue_lock);
+}
+
+void add_proc(struct pcb_t * proc) {
+	pthread_mutex_lock(&queue_lock);
+	enqueue(&ready_queue, proc);
+	pthread_mutex_unlock(&queue_lock);
+}
+
+
